4e22cd74511383bafb0350aa7c7d7a3c45cef28a
Simon Montoya <simonmontoya19@gmail.com>
2017-11-28 22:04:44 -0500

    create test unit for validation that notification create sussesfuly

--- app/Notifications/PostCommented.php
+++ app/Notifications/PostCommented.php
@@ -2,7 +2,6 @@
 
 namespace App\Notifications;
 
-use App\User;
 use App\Comment;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
@@ -13,12 +12,7 @@ class PostCommented extends Notification
 {
     use Queueable;
 
-    /**     
-     * @var
-     */
-    public $commentAuthor;
-
-    /**     
+     /**     
      * @var
      */
     public $comment;
@@ -28,9 +22,9 @@ class PostCommented extends Notification
      *
      * @return void
      */
-    public function __construct(User $commentAuthor,Comment $comment)
+    public function __construct(Comment $comment)
     {
-        $this->commentAuthor = $commentAuthor;
+        
         $this->comment = $comment;
     }
 
@@ -54,9 +48,9 @@ class PostCommented extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', 'https://laravel.com')
-                    ->line('Thank you for using our application!');
+            ->subject('Nuevo comentario en: '.$this->comment->post->title)
+            ->line($this->comment->user->name.' escribio un comentario en: '.$this->comment->post->title)
+            ->action('Ver post', $this->comment->post->url);
     }
 
     /**

e045045ef728d24183a862ed542244be7514fb4f
Simon Montoya Palacio <simon.montoya@ubiquom.com>
2018-04-18 09:40:11 -0500

    a user can voted positive a post

---
+++ app/Vote.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use App\Post;
+use Illuminate\Database\Eloquent\Model;
+
+class Vote extends Model
+{
+    protected $guarded = [];
+
+    public static function upvote(Post $post)
+    {
+        static::create([
+            'user_id' => auth()->id(),
+            'post_id' => $post->id,
+            'vote' => 1,
+        ]);
+
+        $post->score = 1;
+        $post->save();
+    }
+}

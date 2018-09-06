df72e01446907335688460e4108e954ad265f1e9
Simon Montoya Palacio <simon.montoya@ubiquom.com>
2018-09-05 22:47:06 -0500

    fixed in continuos integration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,9 +27,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment('local', 'testing','Foro')) {
+        /* if ($this->app->environment('local', 'testing','Foro')) {
             $this->app->register(DuskServiceProvider::class);
-        }
+        } */
 
         Carbon::setLocale(config('app.locale'));
 
